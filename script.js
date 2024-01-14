f81fdf75d7f8cc23f5bb12bdc630140ebd106d2a
Jian Wang <z5482749@ad.unsw.edu.au>
2024-01-15 02:25:18 +1100

    added a footer

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function createStars() {
-  const numberOfStars = 100;
+  const numberOfStars = 125;
 
   for (let i = 0; i < numberOfStars; i++) {
     const star = document.createElement('div');
     star.className = 'star';
 
     const posX = Math.random() * window.innerWidth;
-    const posY = Math.random() * window.innerHeight;
+    const posY = Math.random() * document.documentElement.scrollHeight;
     const delay = Math.random() * 6;
     star.style.left = `${posX}px`;
     star.style.top = `${posY}px`;
@@ -25,6 +25,8 @@ function createStars() {
 function stopStars() {
   const stars = document.querySelectorAll('.star');
   stars.forEach(star => star.remove());
+  document.body.classList.add('fade-out');
+  document.querySelector('footer').classList.add('fade-in');
 }
 
 document.addEventListener('click', stopStars);
