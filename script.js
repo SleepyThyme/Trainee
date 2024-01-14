e66ccb35d2a6f9717238bd10cef656e2a5b34ae0
Jian Wang <z5482749@ad.unsw.edu.au>
2024-01-15 01:46:00 +1100

    moved script to script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+  createStars();
+  setTimeout(function () {
+  }, 5000);
+});
+
+function createStars() {
+  const numberOfStars = 100;
+
+  for (let i = 0; i < numberOfStars; i++) {
+    const star = document.createElement('div');
+    star.className = 'star';
+
+    const posX = Math.random() * window.innerWidth;
+    const posY = Math.random() * window.innerHeight;
+    const delay = Math.random() * 6;
+    star.style.left = `${posX}px`;
+    star.style.top = `${posY}px`;
+    star.style.animationDelay = `-${delay}s`;
+
+    document.body.appendChild(star);
+  }
+}
+
+function stopStars() {
+  const stars = document.querySelectorAll('.star');
+  stars.forEach(star => star.remove());
+}
+
+document.body.addEventListener('click', stopStars);
